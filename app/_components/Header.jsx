fe0bf9850d2fb6916112b17c33cc1bb72e7a0e71
Kris To <phuocbao.work@gmail.com>
2025-03-22 17:31:19 -0400

    Authentication (Firebase)

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,24 +1,40 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import ThemeToggle from '@/components/ui/themeToggle'
+import Authentication from './Authentication'
+import { useAuthContext } from '../theme-provider'
+import Link from 'next/link'
 
 function Header() {
-  return (
-    <div className='p-4 flex items-center justify-between'>
-      {/* Logo & Title */}
-      <div className='flex items-center gap-2'>
-        <Image src={'/logo.svg'} alt="Logo" width={50} height={50} />
-        <h2 className='text-2xl font-bold'>Youtube Short Gen</h2>
-      </div>
+    const { user } = useAuthContext();
+    return (
+        <div className='p-4 flex items-center justify-between'>
+            {/* Logo & Title */}
+            <div className='flex items-center gap-2'>
+                <Image src={'/logo.svg'} alt="Logo" width={50} height={50} />
+                <h2 className='text-2xl font-bold'>Youtube Short Gen</h2>
+            </div>
 
-      {/* Right Section - Toggle & Sign In */}
-      <div className='flex items-center gap-5'>
-        <ThemeToggle/>
-        <Button>Sign In</Button>
-      </div>
-    </div>
-  )
+            {/* Right Section - Toggle & Sign In */}
+            <div className='flex items-center gap-5'>
+                <ThemeToggle/>
+                {!user?
+                    <Authentication>
+                        <Button>Sign In</Button>
+                    </Authentication>
+                    : 
+                    <div className='flex items-center gap-2'>
+                    <Link href="/dashboard">
+                    <Button>Dashboard</Button>
+                    </Link>
+                    <Image src={user?.photoURL} alt="User" width={40} height={40} className='rounded-full'/>
+                    </div>
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Header
--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,13 +1,28 @@
+'use client'
+import Link from 'next/link'
+import { useAuthContext } from '../theme-provider'
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import Authentication from './Authentication'
 
 function Hero() {
+    const { user } = useAuthContext();
   return (
     <div className='py-12 flex flex-col gap-4 items-center
      justify-center my-12 md:px-16 lg:px-24 xl:px-32'>
-      <h2 className='text-5xl font-bold text-center'>AI Youtube Short Generator</h2>
-      <p className='text-2xl text-muted-foreground my-4 text-center max-w-2xl'>AI-generated shorts with scripts, images, and audio tracks. Beginner-friendly for your early Youtube career and creation.</p>
-      <div className='flex items-center'><Button size='lg'>Get Started</Button></div>
+        <h2 className='text-5xl font-bold text-center'>AI Youtube Short Generator</h2>
+        <p className='text-2xl text-muted-foreground my-4 text-center max-w-2xl'>AI-generated shorts with scripts, images, and audio tracks. Beginner-friendly for your early Youtube career and creation.</p>
+        <div className='flex items-center'>
+            {user ? (
+                <Link href="/dashboard">
+                    <Button size="lg">Go to Dashboard</Button>
+                </Link>
+                ) : (
+                <Authentication>
+                    <Button size="lg">Get Started</Button>
+                </Authentication>
+                )}
+        </div>
     </div>
   )
 }
